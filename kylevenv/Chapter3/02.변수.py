247cf54db901160d6b126b9c2a4048054e1296e0
Kyle <ghd6840@naver.com>
2021-09-07 22:34:23 +0900

    study python Chapter 3,4

---
+++ kylevenv/Chapter3/02.변수.py
@@ -0,0 +1,12 @@
+# 변수
+
+#변수 데이터 저장
+name = "Magento"
+main_language = "PHP"
+sub_lang = "Javascript,Jquery,Knockout.js,Vue.js,React.js"
+print(name, main_language, sub_lang)
+
+# 변수 데이터 변경
+main_language = "PHP + HTML + XML"
+
+print(name, main_language, sub_lang)
