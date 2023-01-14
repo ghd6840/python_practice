fa89e85def5f24b3543aa1438b1cbe895f62f32f
Kyle <ghd6840@naver.com>
2023-01-14 23:42:12 +0900

    practice database

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # python_practice
 study and simple project
+- practice python -> Syntax, Class, Connect Database and so on.
+- simple calculator
+- Crawling
+
+# SQLite 사용 테스트
---
+++ kylevenv/advanced_database/sql.py
@@ -0,0 +1,23 @@
+# DDL (CREATE, DROP, ALTER)
+
+# create table (id integer primary key, title, content, created_at)
+
+# drop table
+
+# alter table rename to new name
+# alter table rename column to new name
+
+
+
+# DML (Insert, Select, Update, Delete)
+
+# insert into table(column1, column2) values(value1, value2)
+
+# select *from table
+# select column from table
+
+# update table set column=value, where column=value
+
+# delete from table where column=value
+
+# order by, group by, join, truncate, and so on => too basic.
